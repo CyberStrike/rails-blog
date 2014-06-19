842e12aee09504d9d6c6bd5aef545c04e300c321
Chris Scott <Chris@Gnmlbs.com>
2014-06-18 23:45:59 -0400

    added comment partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,17 +24,9 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
   end
 
   # PATCH/PUT /comments/1
